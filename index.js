753534a5f79c773bc7a9e7a15f0e020014addaf6
mrscottiefy <scottnathaniel.lim@gmail.com>
2021-07-30 21:19:11 +0800

    working copy that sends encrypted data to slack, no decryption yet

--- index.js
+++ index.js
@@ -1,12 +1,60 @@
+const webhookUrl = "https://hooks.slack.com/services/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"
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
+
+async function readRequestBody(request) {
+  const { headers } = request
+  const contentType = headers.get("content-type") || ""
+
+  if (contentType.includes("application/json")) {
+    return JSON.stringify(await request.json())
+  }
+  else if (contentType.includes("application/text")) {
+    return request.text()
+  }
+  else if (contentType.includes("text/html")) {
+    return request.text()
+  }
+  else if (contentType.includes("form")) {
+    const formData = await request.formData()
+    const body = {}
+    for (const entry of formData.entries()) {
+      body[entry[0]] = entry[1]
+    }
+    return JSON.stringify(body)
+  }
+  else {
+    const myBlob = await request.blob()
+    const objectURL = URL.createObjectURL(myBlob)
+    return objectURL
+  }
+}
+
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  if (request.method === "POST") {
+    return await processRequest(request);
+  }else if(request.method === "GET") {
+    return new Response("Expected POST", { status: 500 })
+  }
+}
+
+async function processRequest(request){
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type':'application/json'
+    },
+    body: JSON.stringify(
+      { 
+        "text": await readRequestBody(request)
+      }
+    )
+  }
+  return await fetch(webhookUrl,options);
 }
