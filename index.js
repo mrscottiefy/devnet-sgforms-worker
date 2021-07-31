e9db45de1bbc7e8e8c6a51a8e1985d326dfdd365
mrscottiefy <scottnathaniel.lim@gmail.com>
2021-07-31 12:15:48 +0800

    decrypted response successful

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const formsg = require('@opengovsg/formsg-sdk')({
-  mode: 'production',
-})
-const slackWebHookUrl = "https://hooks.slack.com/services/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"
+const formsg = require('@opengovsg/formsg-sdk')()
+const postURL = `${POST_URL}`
 const formSecretKey  = `${FORM_KEY}`
 const HAS_ATTACHMENTS = false
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
-})
+});
 
 async function readRequestBody(request) {
   const { headers } = request
@@ -42,37 +40,80 @@ async function readRequestBody(request) {
  * @param {Request} request
  */
 async function handleRequest(request) {
+  //* Logging
+  // sendSlackString("CALLING URL: " + request.url);
+
   if (request.method === "POST") {
-    return await processRequest(request);
+    return await processRequest(await readRequestBody(request));
   }else if(request.method === "GET") {
     return new Response("Expected POST", { status: 500 })
   }
 }
 
 async function processRequest(request){
-  console.log(request.headers)
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type':'application/json'
-    },
-    body: JSON.stringify(
-      { 
-        "text": await decryptRequest(request)
-      }
-    )
+   //* Logging
+  // sendSlackString("processRequest: " + JSON.stringify(request));
+  let decryptedRequest = await decryptRequest(request)
+  // let filteredText = await filterRequest(decryptedRequest)
+  if (decryptedRequest){
+    //* Logging
+    sendSlackString("decryptedResponse: " + JSON.stringify(decryptedRequest));
+      const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify(
+        { 
+          "text": decryptedRequest
+        }
+      )
+    }
+   return await fetch(postURL,options);
   }
-  return await fetch(slackWebHookUrl,options);
+  else{
+    setTimeout(500)
+    return JSON.stringify({})
+  }
 }
 
 async function decryptRequest(request){
-
-  const submission = HAS_ATTACHMENTS ? await formsg.crypto.decryptWithAttachments(formSecretKey, request) : formsg.crypto.decrypt(formSecretKey, request)
-
-  if (submission) {
-    return readRequestBody(submission);
-  } else {
-    return "Could not decrypt the submission"
-  }
-
+  //* Logging
+  // sendSlackString("decryptingRequest: " + JSON.stringify(request))
+  return formsg.crypto.decrypt(formSecretKey, JSON.parse(request).data)
 }
+
+
+/* 
+* Logging Function - Sends text to Slack 
+*/
+async function sendSlackString(text) {
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(
+      {
+        "text": text
+      }
+    )
+  }
+  return await fetch(postURL, options);
+}
+
+/* 
+@response is an array of objects 
+*/
+// async function filterRequest(response){
+//   let answers = await response.filter((q) => {
+//     return q.answer != ""
+//   })
+//   let text = ""
+//   answers.forEach(qna => {
+//     text += qna.question + ": " + qna.answer + `\n`
+//   });
+
+//   return text
+// }
